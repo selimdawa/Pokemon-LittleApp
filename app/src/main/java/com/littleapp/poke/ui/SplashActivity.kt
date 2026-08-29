1a329e8301c222bbe7587471c6969ab62f9147dc
selimdawa <selimdawa@gmail.com>
2026-08-29 06:57:00 +0300

    Edge to Edge - Add

--- app/src/main/java/com/littleapp/poke/ui/SplashActivity.kt
+++ app/src/main/java/com/littleapp/poke/ui/SplashActivity.kt
@@ -1,7 +1,10 @@
 package com.littleapp.poke.ui
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
 import com.littleapp.poke.databinding.ActivitySplashBinding
 import com.littleapp.poke.ui.view.MainActivity
@@ -19,9 +22,15 @@ class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         applyAppTheme()
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         lifecycleScope.launch {
             delay(2000.milliseconds)
